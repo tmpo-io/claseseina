52375ec63571c4b3a79c2eb1ad086ecf93acf2ea
jordi collell <jordic@gmail.com>
2015-04-30 11:12:22 +0200

    mes coses per la clase

--- clase10.md
+++ clase10.md
@@ -1,6 +1,58 @@
 ## CLASE 10
 
-Fiiiin!
+**Fiiiin!**  :laughing:
+
 Proyecto final. Vamos a confeccionar una herramienta para mostrar portafolios.
 
 
+- Nuestro visor de portafolios le llamaremos... 
+- Contendrá dos vistas básicas:
+	A. Indice de Proyectos
+	B. Galería de Cada Proyecto.
+	
+- Para hacer las tareas de mantenimiento fáciles, actualizar los proyectos de forma simple, haremos que la lista y las imágenes de estos se construyan de forma dinámica a partir de la información que hay de carpetas. Así, um proyecto, será una carpeta, en la forma:
+
+
+/<raiz>
+	
+	00_proyecto1/
+		
+		00_imagen1.jpg
+		01_imagen2.jpg
+		02_imagen3.jpg
+		
+	00_proyecto2/
+		
+		00_ficha1.jpg
+		00_ficha3.jpg
+		
+
+Nosotros crearemos dos php. 
+
+	1. index.php. 
+		Generará un indice de proyectos + La información que deseemos de los mismos.
+	2. view.php
+		Nos mostrará cada una de las fichas de proyecto, con toda la galería de imágenes.
+	
+	El segundo script, recibirá como parámetro el nombre de la carpeta, en formato:
+	view.php?proyecto=00_proyecto
+	
+
+
+Para poder generar nuestros portafolios, debermos usar la siguiente librería de funciones:
+
+[funcs.php](clase10/funcs.php)
+
+
+
+
+
+
+
+
+
+
+
+
+
+Ha sido un placer compartir estas clases con vosotros :blue_heart: :blue_heart:
--- clase10/funcs.php
+++ clase10/funcs.php
@@ -54,6 +54,8 @@ function GetImages($path, $only_one=false) {
 	return $res;
 }
 
+// Devuelve un mapa[path]imagen
+// Para poder generar la lista de imagenes de nuestras galerias.
 
 function GetImageForDir($path) {
 	$dirs = GetDirs($path);
@@ -74,6 +76,25 @@ function GetImageForDir($path) {
 }
 
 
+// Comprueba si una carpeta "proyecto" contiene un archivo de data, 
+// que haya que adjuntar al proyecto...
+
+function HasData($dir) {
+
+	// normalize path
+	if( !startsWith($dir, "/") ) {
+		$currentPath = dirname(__FILE__);
+		$dir = $currentPath . DIRECTORY_SEPARATOR . $dir;
+	}
+	
+	if( file_exists( $dir . "/data.php" ) ) 
+		return true;
+	
+	return false;
+
+}
+
+
 
 function endsWith($haystack, $needle) {
     // search forward starting from end minus needle length characters
@@ -82,9 +103,14 @@ function endsWith($haystack, $needle) {
     		strpos($haystack, $needle, $temp) !== FALSE);
 }
 
+function startsWith($haystack, $needle) {
+    // search backwards starting from haystack length characters from the end
+    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+}
+
+
 
 /*
-
 echo "Dirname: " . dirname(__FILE__);
 
 echo "dirs: " . join(", ", GetDirs('/home/jordi/')) . "\n";
@@ -99,5 +125,7 @@ $desktop = GetImageForDir( '/home/jordi/Escriptori/' );
 var_dump( $desktop );
 
 echo "\n";
+
+var_dump( HasData( 'test' ) );
 
 */
