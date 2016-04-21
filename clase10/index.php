83606e9c8460d2fc13f36ecf6cd063a23d2def71
oriol <oriolc@gmail.com>
2016-04-21 18:09:51 +0200

    afegeixo random de colors

--- clase10/index.php
+++ clase10/index.php
@@ -2,10 +2,14 @@
   <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/estilos.css" type="text/css" />
+    <meta http-equiv="refresh" content="0.5">
   </head>
   <body>
+    <?php
+    $colores = array("black", "yellow", "blue", "red", "pink", "orange");
+    ?>
     <?php for($i=0;$i<160;$i++) { ?>
-    <div class="box">
+    <div class="box" style="background-color: <?php echo $colores[ rand(0,5) ]; ?>">
     </div>
     <?php } ?>
   </body>
