09c07928d787fe81246778053af3c44aa8b7676b
oriol <oriolc@gmail.com>
2016-04-21 17:32:59 +0200

    afegeixo exercici 10

---
+++ clase10/ejemplofor.php
@@ -0,0 +1,9 @@
+<?php
+for($i=0;$i<1000;$i++) {
+?>
+Un texto
+<img src="" />
+<h2>Holaaa</h2>
+<?php
+}
+?>
---
+++ clase10/index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/estilos.css" type="text/css" />
+  </head>
+  <body>
+    <?php for($i=0;$i<160;$i++) { ?>
+    <div class="box">
+    </div>
+    <?php } ?>
+  </body>
+</html>
