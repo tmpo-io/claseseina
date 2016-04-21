8f52532aaf407cf2c952f964d9d526c0f0dac9e6
oriol <oriolc@gmail.com>
2016-04-21 19:03:34 +0200

    afegeixo el reveal

--- clase10/rancomColores.php
+++ clase10/rancomColores.php
@@ -2,9 +2,9 @@
   <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/estilos.css" type="text/css" />
-    <meta http-equiv="refresh" content="0.5">
   </head>
   <body>
+    <div class="container">
     <?php
     ?>
     <?php for($i=0;$i<160;$i++) { ?>
@@ -17,5 +17,17 @@
     <div class="box" style="background-color:rgba(<?php echo $r ?>, <?php echo $g ?>, <?php echo $b ?>, <?php echo $a ?>)">
     </div>
     <?php } ?>
+
+    </div>
+
+    <script src="scrollreveal/dist/scrollreveal.js"></script>
+    <script>
+    window.sr = ScrollReveal({
+      reset: true,
+      container: document.querySelector('.container'),
+    });
+    sr.reveal('.box');
+    </script>
+
   </body>
 </html>
